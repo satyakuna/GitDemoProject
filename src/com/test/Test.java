22dde24269f35cc330b098d15a9e3f25f2061c8f
Satya <snmohapatra91@gmail.com>
2024-09-19 16:52:14 +0530

    first commit

---
+++ src/com/test/Test.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if (hungry=="yes") {
+			System.out.println("Take some food..");
+		}else {
+			System.out.println("Do work..");
+		}
+		
+			
+		}
+
+}
